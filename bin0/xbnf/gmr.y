2829508d1de170deff62f24096a8445d85b4eb39
Stephen Taylor <stnh.email@icloud.com>
2021-05-30 16:00:51 -0400

    added xbnf parser

---
+++ bin0/xbnf/gmr.y
@@ -0,0 +1,9 @@
+%{
+  #define YYDEBUG 1
+  int yylex(void);
+  void yyerror(char *s);
+%}
+%token X00
+%%
+P : X00 '\x22' '\x5d' ;
+
---
+++ scripts/backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ $# != 0 ] ; then
+		echo "usage: backup.sh"
+		exit -1
+fi
+if [ ! -d "./.backup" ] ; then
+		mkdir ./.backup
+		echo "[created .backup]"
+fi
+if [ ! -d "./.backup/previous" ] ; then
+		mkdir ./.backup/previous
+		echo "[created .backup/previous]"
+fi
+# save the previous backup
+shopt -s nullglob
+for f in ./.backup/*
+do
+		if [ -f $f ] ; then
+				mv -f $f ./.backup/previous
+		fi
+done
+# copy everything to .backup
+shopt -s nullglob
+for f in *.[ch] *.[ly] *.sh Makefile README
+do
+		if [ -f $f ] ; then
+				echo "[saving $f]"
+				cp -f $f ./.backup
+		fi
+done
