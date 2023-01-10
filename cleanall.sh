2c6bd94b105516cd13466f65a76ceae84c06ee6b
Gunnar Pope <gunnarpope@gmail.com>
2023-01-10 09:31:59 -0500

    update

--- cleanall.sh
+++ cleanall.sh
@@ -33,7 +33,7 @@ make ${MODE} clean
 popd
 
 # make the hmr grammars
-for DIR in gmr0 gmr1 jnum jstring json bin0 bin1 bin2 unicode command response json.unicode json.unicode.combinator
+for DIR in gmr0 gmr1 jnum jstring json bin0 bin1 bin2 unicode command response json.unicode json.unicode.combinator jnum3
 do
 		if [ -d ./${DIR}/bison ] ; then
 				pushd ./${DIR}/bison
