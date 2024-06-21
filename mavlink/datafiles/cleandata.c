c1c22d10c6edfd6f6f032797ce38b4546d12fe32
Josh Meise <joshua.m.meise.24@dartmouth.edu>
2024-06-21 12:58:27 -0400

    data almost cleaned - still need to find out why it corrupts ater deleting MDNS message

--- mavlink/datafiles/cleandata.c
+++ mavlink/datafiles/cleandata.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 typedef struct message {
 	unsigned char header[50];
@@ -34,12 +35,27 @@ void printMessage(message_t mess, int len) {
 	printf("\n");
 }
 
+void writeMessageToFile(message_t mess, int len, FILE *ofile) {
+	int i;
+	unsigned char buf;
+	printf("Length to write = %d\n\n", len);
+	for (i = 0; i < len; i++) {
+		
+		if (i < 32) buf = mess.header[i];
+		else buf = mess.body[i - 32];
+
+		if (fwrite(&buf, sizeof(buf), 1, ofile) != 1)
+			printf("Message write failed.\n");
+	}
+}
+
 int main(int argc, char **argv) {
-	FILE *ifile;
+	FILE *ifile, *ofile;
 	unsigned char buf;
 	int bytesRead, i, len, prevLen;
 	message_t mess;
 	bool prevMDNS;
+	char *fname;
 	
 	if (argc != 2) {                                                                                                                                                                                         
     printf("usage: read fileToRead\n");
@@ -51,13 +67,28 @@ int main(int argc, char **argv) {
     exit(EXIT_FAILURE);
 	}
 
+	fname = (char *)malloc(sizeof(char)*(strlen(argv[1]) + 1 + strlen("clean")));
+
+	sprintf(fname, "clean%s", argv[1]);
+	
+	if ((ofile = fopen(fname, "wb")) == NULL) {
+    printf("File not opened successfully\n");
+    exit(EXIT_FAILURE);
+	}
+
+	free(fname);
+
 	i = 0;
 
 	printf("Header: ");
 	while(i < 40) {
-		bytesRead = fread(&buf, 1, sizeof(buf), ifile);
+		bytesRead = fread(&buf, sizeof(buf), 1, ifile);
 		printf("%02x ", buf);
 		i++;
+
+		if (fwrite(&buf, sizeof(buf), 1, ofile) != 1)
+			printf("Not successfully written");
+		
 	}
 	printf("\n");
 	
@@ -66,7 +97,7 @@ int main(int argc, char **argv) {
 	prevLen = 0;
 	prevMDNS = false;
 	
-	while ((bytesRead = fread(&buf, 1, sizeof(buf), ifile)) > 0) {
+	while ((bytesRead = fread(&buf, sizeof(buf), 1, ifile)) > 0) {
 		if ((prevLen == 1366 || prevLen == 148 || prevLen == 1440) && prevMDNS == true) {
 			if (i < 52) mess.header[i] = buf;
 			else if (i == 52) {
@@ -86,7 +117,11 @@ int main(int argc, char **argv) {
 		
 		i++;
 		if (((i == (48 + len) && prevMDNS == false) || (i == (68 + len) && prevMDNS == true)) && len != 0) {
-			printMessage(mess, len + 48);
+			printMessage(mess, len + 32);
+
+			if (mess.body[0] == 0xfd)
+				writeMessageToFile(mess, len + 48, ofile);
+			
 			i = 0;
 			prevLen = len;
 			len = 0;
@@ -97,6 +132,8 @@ int main(int argc, char **argv) {
 		}
 
 	}
-	
+
+	fclose(ifile);
+	fclose(ofile);
 	exit(EXIT_SUCCESS);
 }
