1f28320b5b15aa3c904e491af4be7930a26ab63b
Josh Meise <joshua.m.meise.24@dartmouth.edu>
2024-07-24 10:29:02 -0400

    standalone message grammar and tests adjusted

--- mavlink_standalone_message/tests/tv.c
+++ mavlink_standalone_message/tests/tv.c
@@ -1,221 +1,31 @@
 /* 
- * tv.c --- 
+ * tv.c -- creates test cases for a given message based on its message id
+ *         creates passing and failing tests for every byte value in every payload field, as well as header field
  * 
- * Author: Stephen Taylor, Brandon Guzman, Josh Meise
- * Created: 12-02-2020
+ * Author: Joshua M. Meise
+ * Created: 07-16-2024
  * Version: 1.0
- * 
- * Description: 
- * 
  */
 
-//generate good and bad inputs
-
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <mavlink.h>
 
-static void writeSPFile(message_t *mess, FILE *fp, int len) {
-	int i;
-	uint8_t buf;
+int main(int argc, char **argv) {
+	// Variable declarations.
+	int id;	
 
-	i = 0;
-	
-	while (i < len) {
-		if (i < 4) buf = mess->header.timeS[i];
-		else if (i < 8) buf = mess->header.timeUNS[i - 4];
-		else if (i < 12) buf = mess->header.capturedPacketLength[i - 8];
-		else if (i < 16) buf = mess->header.originalPacketLength[i - 12];
-		else if (i < 20) buf = mess->header.messageFam[i - 16];
-		else if (i == 20) buf = mess->header.iphl;
-		else if (i == 21) buf = mess->header.dsf;
-		else if (i < 24) buf = mess->header.totalLength[i - 22];
-		else if (i < 26) buf = mess->header.id[i - 24];
-		else if (i < 28) buf = mess->header.ffOffset[i - 26];
-		else if (i == 28) buf = mess->header.ttl;
-		else if (i == 29) buf = mess->header.protocol;
-		else if (i < 32) buf = mess->header.headerChecksum[i - 30];
-		else if (i < 36) buf = mess->header.sourceAddr[i - 32];
-		else if (i < 40) buf = mess->header.destAddr[i - 36];
-		else if (i < 42) buf = mess->header.sourcePort[i - 40];
-		else if (i < 44) buf = mess->header.destPort[i - 42];
-		else if (i < 46) buf = mess->header.length[i - 44];
-		else if (i < 48) buf = mess->header.checkSum[i - 46];
-		else if (i == 48) buf = mess->body.mavCode;
-		else if (i == 49) buf =	mess->body.payloadLen;
-		else if (i == 50) buf = mess->body.incompFlag;
-		else if (i == 51) buf = mess->body.compFlag;
-		else if (i == 52) buf = mess->body.packetSeq;
-		else if (i == 53) buf = mess->body.systemID;
-		else if (i == 54) buf = mess->body.compID;
-		else if (i < 58) buf = mess->body.messageID[i - 55];		
-		else if (i < 58 + 16) buf = mess->body.payload[i - 58];
-		else if (i < 60 + 16) buf = mess->body.crc[i - 58 - 16];
-		else buf = 0x21;
-		
-		i++;
-
-		fwrite(&buf, sizeof(buf), 1, fp);
-
+	// Check number of arguments.
+	if (argc != 2) {
+		printf("usage: tv msgID\n");
+		exit(EXIT_FAILURE);
 	}
-}
 
-int main(void) {
-	FILE *fp;
-	messArr_t *messArr;
-	uint8_t old;
-	int i;
-	char fname[10];
-	
-	if ((fp = fopen("pass.1", "rb")) == NULL)
-		exit(EXIT_FAILURE);
+	// Convert string to integer.
+	id = atoi(argv[1]);
 
-	messArr = readFile(fp);
+	// Generate test cases for the given message id.
+	generateTests(id);
 
-	fclose(fp);
-
-	if ((fp = fopen("./fail.2", "wb")) == NULL)
-		exit(EXIT_FAILURE);
-
-	old = messArr->messages[0].body.mavCode;
-	messArr->messages[0].body.mavCode = 0x00;
-	
-	writeToFile(messArr->messages[0], fp);
-	
-	fclose(fp);
-
-	messArr->messages[0].body.mavCode = old;
-	
-	if ((fp = fopen("./fail.3", "wb")) == NULL)
-		exit(EXIT_FAILURE);
-
-	old = messArr->messages[0].body.messageID[0];
-	messArr->messages[0].body.messageID[0] = 0x21;
-	
-	writeSPFile(messArr->messages, fp, toInt24le(messArr->messages[0].header.capturedPacketLength) + 16);
-	
-	fclose(fp);
-
-	messArr->messages[0].body.messageID[0] = old;
-
-	if ((fp = fopen("./fail.4", "wb")) == NULL)
-		exit(EXIT_FAILURE);
-
-	old = messArr->messages[0].body.payloadLen;
-	messArr->messages[0].body.payloadLen = 0x21;
-	
-	writeSPFile(messArr->messages, fp, toInt24le(messArr->messages[0].header.capturedPacketLength) + 16);
-	
-	fclose(fp);
-
-	messArr->messages[0].body.payloadLen = old;
-
-	if ((fp = fopen("./fail.5", "wb")) == NULL)
-		exit(EXIT_FAILURE);
-
-	writeSPFile(messArr->messages, fp, toInt24le(messArr->messages[0].header.capturedPacketLength) + 16 - 1);
-	
-	fclose(fp);
-	
-	if ((fp = fopen("./fail.6", "wb")) == NULL)
-		exit(EXIT_FAILURE);
-
-	writeSPFile(messArr->messages, fp, toInt24le(messArr->messages[0].header.capturedPacketLength) + 16 + 1);
-	
-	fclose(fp);
-
-	if ((fp = fopen("./fail.7", "wb")) == NULL)
-		exit(EXIT_FAILURE);
-
-	old = messArr->messages[0].header.messageFam[0];
-	messArr->messages[0].header.messageFam[0] = 0x03;
-	
-	writeSPFile(messArr->messages, fp, toInt24le(messArr->messages[0].header.capturedPacketLength) + 16);
-	
-	fclose(fp);
-
-	messArr->messages[0].header.messageFam[0] = old;
-
-	if ((fp = fopen("./fail.8", "wb")) == NULL)
-		exit(EXIT_FAILURE);
-
-	old = messArr->messages[0].header.iphl;
-	messArr->messages[0].header.iphl = 0x03;
-	
-	writeSPFile(messArr->messages, fp, toInt24le(messArr->messages[0].header.capturedPacketLength) + 16);
-	
-	fclose(fp);
-
-	messArr->messages[0].header.iphl = old;
-
-	if ((fp = fopen("./fail.9", "wb")) == NULL)
-		exit(EXIT_FAILURE);
-
-	old = messArr->messages[0].header.totalLength[0];
-	messArr->messages[0].header.totalLength[0] = 0x03;
-	
-	writeSPFile(messArr->messages, fp, toInt24le(messArr->messages[0].header.capturedPacketLength) + 16);
-	
-	fclose(fp);
-
-	messArr->messages[0].header.totalLength[0] = old;
-
-	if ((fp = fopen("./fail.10", "wb")) == NULL)
-		exit(EXIT_FAILURE);
-
-	old = messArr->messages[0].header.ttl;
-	messArr->messages[0].header.ttl = 0x03;
-	
-	writeSPFile(messArr->messages, fp, toInt24le(messArr->messages[0].header.capturedPacketLength) + 16);
-	
-	fclose(fp);
-
-	messArr->messages[0].header.ttl = old;
-
-	if ((fp = fopen("./fail.11", "wb")) == NULL)
-		exit(EXIT_FAILURE);
-
-	old = messArr->messages[0].header.sourcePort[0];
-	messArr->messages[0].header.sourcePort[0] = 0x03;
-	
-	writeSPFile(messArr->messages, fp, toInt24le(messArr->messages[0].header.capturedPacketLength) + 16);
-	
-	fclose(fp);
-
-	messArr->messages[0].header.sourcePort[0] = old;
-
-	if ((fp = fopen("./fail.12", "wb")) == NULL)
-		exit(EXIT_FAILURE);
-
-	old = messArr->messages[0].header.destPort[0];
-	messArr->messages[0].header.destPort[0] = 0x03;
-	
-	writeSPFile(messArr->messages, fp, toInt24le(messArr->messages[0].header.capturedPacketLength) + 16);
-	
-	fclose(fp);
-
-	messArr->messages[0].header.destPort[0] = old;
-
-	for (i = 0; i < 16; i++) {
-		sprintf(fname, "./pass.%d", i + 3);
-
-		if ((fp = fopen(fname, "wb")) == NULL)
-			exit(EXIT_FAILURE);
-
-		old = messArr->messages[0].body.payload[i];
-		messArr->messages[0].body.payload[i] = old + 0x01;
-		
-		writeToFile(messArr->messages[0], fp);
-
-		fclose(fp);
-
-		messArr->messages[0].body.payload[i] = old;
-	}
-	
-	freeMem(messArr);
-	
 	exit(EXIT_SUCCESS);
-
 }
