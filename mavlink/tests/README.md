fa5d19121e21fae5fc1e31c304fc6bae0edfd899
Josh Meise <joshua.m.meise.24@dartmouth.edu>
2024-08-02 12:27:28 -0400

    test files added

--- mavlink/tests/README.md
+++ mavlink/tests/README.md
@@ -2,12 +2,14 @@
 
 Tests for the MAVLink parser which contains tests for all given byte values in all fields of all messages.
 
-Note: It is best to test this parser using the `mavlinktest.sh` script in the `xbnf`/`bison` directories due to the large number of test files produced when running `tv.sh`.
+Note: If testing all fields, it is best to test this parser using the `mavlinktest.sh` script in the `xbnf`/`bison` directories due to the large number of test files produced when running `tv.sh`.
 
 ## Contents:
 
+* `pass.1` and `pass.2` are passing tests for two complete flights with the drone (contain multiple message types)
+* `pass.3` - `pass.n` are passing tests for each individual message for two complete flights flights with the drone
 * `tv.c` auto-generates a range of tests, testing all ranges of byte values for all fields
-* `tv.sh` creates all of the below-described test files using `tv.c`
+* `tv.sh [-l]` creates all of the above- and below-described test files using `tv.c` if using the `-l` flag; generates only the above-described tests if not using the '-l' flag
 * `pass.msgID.len` is a test for each possible payload length with random values assigned to wildcard fields and valid values assigned to enumerated fields 
 * `pass.msgID.len.ind.val` is a passing test for a given mesage ID at its maximum payload length (all fields present) where `ind` is the index of the payload field being tested and `val` is the byte value being tested
 * `fail.msgID.len.ind.val` is a failing test for a given mesage ID at its maximum payload length (all fields present) where `ind` is the index of the payload field being tested and `val` is the byte value being tested
@@ -15,12 +17,8 @@ Note: It is best to test this parser using the `mavlinktest.sh` script in the `x
 * `pass.msgID.len.header.field.val` is a passing test for a given mesage ID at its maximum payload length (all fields present) in the header field where `field` is the name of the header field being tested and val is the byte value being tested
 * `fail.msgID.len.extraByte` and `fail.msgID.len.oneByteLess` are failing tests for messages of the maximum length with one byte too many and one byte too few respectively
 * `pass.msgID.len.crcx.val` are passing tests for messages of the maximum length with `crc` field `x` changed such that it contains a byte of value `val` 
-* `pass.1` - `pass.n` are passing tests for each individual message for two complete flights flights with the drone
-* `pass.10000` and `pass.10001` are passing tests for two complete flights with the drone (contain multiple message types)
-* `pass.10002` is an empty file
 
 ## Usage:
-
 
 In the `../../mavlink/utils` directory:
 * `make clean`
@@ -29,4 +27,4 @@ In the `../../mavlink/utils` directory:
 Back in the `tests` directory:
 * `make clean`
 * `make`
-* `./tv.sh` to generate all test cases
+* `./tv.sh -l` to generate all test cases
--- mavlink/tests/tv.sh
+++ mavlink/tests/tv.sh
@@ -9,9 +9,17 @@ popd () {
 }
 
 # Check number of arguments
-if [ $# != 0 ]; then
-		echo "usage: tv.sh"
+if [ $# != 0 ] && [ $# != 1 ] ; then
+		echo "usage: tv.sh [-l]"
 		exit
+fi
+
+# Check that second argument is -l
+if [ $# == '1' ]; then
+		if [ $1 != '-l']; then
+				echo "usage: tv.sh [-l]"
+				exit
+		fi
 fi
 
 # This is the source directory path.
@@ -21,29 +29,31 @@ SRCDIR="../mavlink_source_files"
 make clean > /dev/null
 make > /dev/null
 
-# Array of message IDs currently in the parser
-msgIDs=(0 1 4 8 22 24 29 30 31 32 33 42 46 65 74 77 83 85 87 109 141 147 230 241 242 245 253 340 380 410 411 12901 12904)
-
-# Loop through all message IDs
-for id in "${msgIDs[@]}"; do
-		# Generate the tests for the given message code
-		./tv ${id}
+if [ $# == 1 ]; then
+		# Array of message IDs currently in the parser
+		msgIDs=(0 1 4 8 22 24 29 30 31 32 33 42 46 65 74 77 83 85 87 109 141 147 230 241 242 245 253 340 380 410 411 12901 12904)
 		
-		echo "Tests for message ID ${id} generated"
-done
+		# Loop through all message IDs
+		for id in "${msgIDs[@]}"; do
+				# Generate the tests for the given message code
+				./tv ${id}
+				
+				echo "Tests for message ID ${id} generated"
+		done
+fi
 
 # Create a test file for full flights with the drone with the drone.
 if [ -e ${SRCDIR}/run1.pcap ]; then
 		pushd ${SRCDIR}
 		make clean > /dev/null
 		make > /dev/null
-
+		
 		# Remove PCAP headers and extract only the MAVLink message portion
-		./makemav ./run1.pcap ./pass.10000 > /dev/null
-
+		./makemav ./run1.pcap ./pass.1 > /dev/null
+		
 		# Move to tests directory
-		mv ./pass.10000 ../tests > /dev/null
-
+		mv ./pass.1 ../tests > /dev/null
+		
 		make clean > /dev/null
 		popd
 fi
@@ -53,30 +63,30 @@ if [ -e ${SRCDIR}/run2.pcap ]; then
 		pushd ${SRCDIR}
 		make clean > /dev/null
 		make > /dev/null
-
+		
 		# Remove PCAP headers and extract only the MAVLink message portion
-		./makemav ./run2.pcap ./pass.10001 > /dev/null
-
+		./makemav ./run2.pcap ./pass.2 > /dev/null
+		
 		# Move to tests directory
-		mv ./pass.10001 ../tests > /dev/null
+		mv ./pass.2 ../tests > /dev/null
 		
 		make clean > /dev/null
 		popd
 fi
 
 # Start counter for messages for individual drone flights.
-let msgNum=1
+let msgNum=3
 
 if [ -e ${SRCDIR}/run1.pcap ]; then
 		pushd ${SRCDIR}
 		make clean > /dev/null
 		make > /dev/null
-
+		
 		# Count messages in run1 file
 		./countmessages ./run1.pcap > foo
 		numMess=$(cat foo)
 		rm foo > /dev/null
-
+		
 		# Create a test in a unique file for each message
 		for (( i = 1; i <= numMess; i++ )); do
 				./extractbymessagenumber ./run1.pcap pass.${msgNum} ${i} > /dev/null
@@ -90,7 +100,7 @@ fi
 if [ -e ${SRCDIR}/run2.pcap ]; then
 		pushd ${SRCDIR}
 		make > /dev/null
-
+		
 		# Count messages in run2 file
 		./countmessages ./run2.pcap > foo
 		numMess=$(cat foo)
@@ -108,6 +118,3 @@ fi
 
 # More all of the tests to the tests directory
 mv ${SRCDIR}/pass.* . > /dev/null
-
-# Create an empty file
-touch pass.10002 
